14c1a26cda82082adf989ae5c7b79e1de681978f
Aatish Bhatia <aatish@gmail.com>
2019-04-29 17:50:30 -0400

    Update sketch1.js

--- two-way-binding/sketch1.js
+++ two-way-binding/sketch1.js
@@ -17,6 +17,7 @@ function sketch(parent) {
     };
 
     p.mouseDragged = function() {
+      // check that input came from within this canvas
       if (0 < p.mouseX && p.mouseX < p.width && 0 < p.mouseY && p.mouseY < p.height)
       {
         parent.$emit('update:x', p.mouseX);
