fe210eb5af6cdb2531d356336eda5cba1f3b405d
Aatish Bhatia <aatish@gmail.com>
2019-05-07 16:05:47 -0400

    added link to code

--- two-way-binding/sketch2.js
+++ two-way-binding/sketch2.js
@@ -1,4 +1,7 @@
-function sketch(parent) {
+// this p5 sketch is written in instance mode
+// read more here: https://github.com/processing/p5.js/wiki/Global-and-instance-mode
+
+function sketch(parent) { // we pass the sketch data from the parent
   return function( p ) { // p could be any variable name
     // p5 sketch goes here
 
